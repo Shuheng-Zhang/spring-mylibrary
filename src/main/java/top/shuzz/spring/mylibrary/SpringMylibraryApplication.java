1f9da8a7a52ed4282f5c5154bb61d35c60b18b02
Shuheng-Zhang <zhangshuheng01@163.com>
2024-02-14 11:37:56 +0800

    初始提交

---
+++ src/main/java/top/shuzz/spring/mylibrary/SpringMylibraryApplication.java
@@ -0,0 +1,13 @@
+package top.shuzz.spring.mylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMylibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMylibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/shuzz/spring/mylibrary/SpringMylibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package top.shuzz.spring.mylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMylibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
